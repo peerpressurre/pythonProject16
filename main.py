ec8c6a474bad99568e22e9daf68472a4c7fd2bb3
peerpressurre <gsophiay@gmail.com>
2022-12-03 00:33:57 +0200

    1сm

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-try:
-    text = input("---")
-    somefile = open("hello.txt","w")
-    try:
-        somefile.write(text)
-    except Exception as e:
-        print(e)
-    finally:
-        somefile.close()
-except Exception as ex:
-    print(ex)
+import csv
+accountfile = 'account.csv'
+login = input('enter your login->')
+password = input('enter your password->')
+path = input('enter path of file->')
+
+data = [
+    {'login': login},
+    {'password': password},
+    {'path': path}
+]
+columns = ['login', 'password', 'path']
+# data_str = f'login: {login},password: {password},path: {path}'
+# data_str = data_str.split(',')
+# for key in data_str:
+#     print(key, end='\n')
+
+with open(accountfile, 'w', newline='') as accountfile:
+    writer = csv.DictWriter(accountfile, fieldnames=columns)
+    writer.writeheader()
+    for row in data:
+        writer.writerow(row)
