ceb359b67fd37a7e919a77305e86b266753f09f8
peerpressurre <gsophiay@gmail.com>
2022-12-05 18:19:14 +0200

    промеж

--- main.py
+++ main.py
@@ -1,22 +1,71 @@
 import csv
-accountfile = 'account.csv'
-login = input('enter your login->')
-password = input('enter your password->')
-path = input('enter path of file->')
+try:
+    begin = input('Ви маєте акаунт?(y/n)')
+    datafile = 'data.csv'
+    if begin == 'n':
+        def signup():
+            print('Створення акаунту')
+            loginc = input('Придумайте логін - ')
+            passwordc = input('Придумайте пароль - ')
+            path = input('Введіть шлях ->')
+            csv_columns = [loginc, passwordc, path]
 
-data = [
-    {'login': login},
-    {'password': password},
-    {'path': path}
-]
-columns = ['login', 'password', 'path']
-# data_str = f'login: {login},password: {password},path: {path}'
-# data_str = data_str.split(',')
-# for key in data_str:
-#     print(key, end='\n')
+            try:
+                with open(datafile, 'a') as csvfile:
+                    writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+                    writer.writeheader()
+            except IOError:
+                print('I/P error')
+        signup()
 
-with open(accountfile, 'w', newline='') as accountfile:
-    writer = csv.DictWriter(accountfile, fieldnames=columns)
-    writer.writeheader()
-    for row in data:
-        writer.writerow(row)
+    if begin == 'y':
+        print('Вхід в акаунт')
+        counter = 0
+        while counter < 4:
+            loginw = input('Логін - ')
+            passwordw = input('Пароль - ')
+            if loginw in datafile:
+                if passwordw in datafile:
+                    print('Вхід успішний!')
+                    with open(datafile, 'r') as csvfile:
+                        write = csvfile.readline().split(',')
+                    print(bool(loginw == write[0]))
+                else:
+                    print('Невірний пароль. Спробуйте знову')
+            else:
+                print('Невірний логін. Спробуйте знову')
+            counter += 1
+        print('Максимальна кількість спроб вичерпана. Спробуйте пізніше.')
+
+    product = input('Введіть назву продукту - ')
+    category = input('Введіть назву категорії - ')
+    amount = input('Введіть кількість - ')
+    description = input('Введіть опис - ')
+    price = input('Введіть ціну товара - ')
+    store = input('Введіть магазин купівлі - ')
+    csv_columns1 = ['Продукт', 'Категорія', 'Кількість', 'Опис', 'Ціна', 'Магазин']
+
+    dict_data = [
+        {'Продукт': product, 'Категорія': category, 'Кількість': amount,
+            'Опис': description, 'Ціна': price, 'Магазин': store}
+    ]
+
+    csv_file = "manager.csv"
+
+    try:
+        with open(csv_file, 'w') as csvfile:
+            writer = csv.DictWriter(csvfile, delimiter=';', fieldnames=csv_columns1)
+            writer.writeheader()
+            for data in dict_data:
+                writer.writerow(data)
+    except IOError:
+        print("I/O error")
+    if loginw != write[0]:
+        print('Невірний логін')
+    if passwordw != write[1]:
+        print('Невірний пароль')
+
+    else:
+        print('Перевірте ввод символів!')
+except Exception as e:
+    print(e)
