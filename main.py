19130fc991fd04a87eae6f86e018eeaca9efe27f
peerpressurre <gsophiay@gmail.com>
2022-12-08 13:28:34 +0200

    if n

--- main.py
+++ main.py
@@ -6,74 +6,109 @@ try:
         loginc = input('Придумайте логін - ')
         passwordc = input('Придумайте пароль - ')
         path = input('Введіть шлях ->')
-        csv_columns = [loginc, passwordc, path]
+        signup = [
+            {'login': loginc, 'password': passwordc, 'path': path}
+        ]
+        csv_columns = ['login', 'password', 'path']
         datafile = loginc + '.csv'
 
         try:
-            with open(datafile, 'a') as csvfile:
-                writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+            with open(datafile, 'w') as csvfile:
+                writer = csv.DictWriter(csvfile, delimiter=';', fieldnames=csv_columns)
                 writer.writeheader()
+                for data in signup:
+                    writer.writerow(data)
         except IOError:
             print('I/P error')
         print('Реєстрація пройшла успішно')
 
-    if begin == 'y':
-        print('Вхід в акаунт')
-        counter = 0
-        while counter < 4:
-            loginw = input('Логін - ')
-            passwordw = input('Пароль - ')
-            datafile = loginw + '.csv'
-            print(datafile)
+        product = input('Введіть назву продукту - ')
+        category = input('Введіть назву категорії - ')
+        amount = input('Введіть кількість - ')
+        description = input('Введіть опис - ')
+        price = input('Введіть ціну товара - ')
+        store = input('Введіть магазин купівлі - ')
+        csv_columns1 = ['Продукт', 'Категорія', 'Кількість', 'Опис', 'Ціна', 'Магазин']
 
-            if loginw in datafile:
-                if passwordw in datafile:
-                    print('Вхід успішний!')
-                    with open(datafile, 'r') as csvfile:
-                        write = csvfile.readline().split(',')
-                    print(bool(loginw == write[0]))
-                else:
-                    print('Невірний пароль. Спробуйте знову')
-            else:
-                print('Невірний логін. Спробуйте знову')
-            counter += 1
-        print('Максимальна кількість спроб вичерпана. Спробуйте пізніше.')
-        breakpoint()
+        dict_data = [
+            {'Продукт': product, 'Категорія': category, 'Кількість': amount,
+             'Опис': description, 'Ціна': price, 'Магазин': store}
+        ]
 
-    product = input('Введіть назву продукту - ')
-    category = input('Введіть назву категорії - ')
-    amount = input('Введіть кількість - ')
-    description = input('Введіть опис - ')
-    price = input('Введіть ціну товара - ')
-    store = input('Введіть магазин купівлі - ')
-    csv_columns1 = ['Продукт', 'Категорія', 'Кількість', 'Опис', 'Ціна', 'Магазин']
+        csv_file = '_data.csv'
 
-    dict_data = [
-        {'Продукт': product, 'Категорія': category, 'Кількість': amount,
-            'Опис': description, 'Ціна': price, 'Магазин': store}
-    ]
+        try:
+            with open(csv_file, 'w') as csvfile:
+                writer = csv.DictWriter(csvfile, delimiter=';', fieldnames=csv_columns1)
+                writer.writeheader()
+                for data in dict_data:
+                    writer.writerow(data)
 
-    csv_file = "manager.csv"
+            with open(csv_file, "r", newline="") as file:
+                reader = csv.reader(file)
+                for row in reader:
+                    print(row[0], '-----', row[1], '-----', row[2], '-----', row[3], '-----', row[4], '-----', row[5])
+        except IOError:
+            print("I/O error")
 
-    try:
-        with open(csv_file, 'w') as csvfile:
-            writer = csv.DictWriter(csvfile, delimiter=';', fieldnames=csv_columns1)
-            writer.writeheader()
-            for data in dict_data:
-                writer.writerow(data)
 
-        with open(csv_file, "r", newline="") as file:
-            reader = csv.reader(file)
-            for row in reader:
-                print(row[0], '-----', row[1], '-----', row[2], '-----', row[3], '-----', row[4], '-----', row[5])
-    except IOError:
-        print("I/O error")
-    if loginw != write[0]:
-        print('Невірний логін')
-    if passwordw != write[1]:
-        print('Невірний пароль')
+    # if begin == 'y':
+    #     print('Вхід в акаунт')
+    #     counter = 0
+    #     while counter < 4:
+    #         loginw = input('Логін - ')
+    #         passwordw = input('Пароль - ')
+    #         datafile = loginw + '.csv'
+    #         print(datafile)
+    #
+    #         if loginw in datafile:
+    #             if passwordw in datafile:
+    #                 print('Вхід успішний!')
+    #                 with open(datafile, 'r') as csvfile:
+    #                     write = csvfile.readline().split(',')
+    #                 print(bool(loginw == write[0]))
+    #             else:
+    #                 print('Невірний пароль. Спробуйте знову')
+    #         else:
+    #             print('Невірний логін. Спробуйте знову')
+    #         counter += 1
+    #     print('Максимальна кількість спроб вичерпана. Спробуйте пізніше.')
+    #     breakpoint()
 
-    else:
-        print('Перевірте ввод символів!')
+    # product = input('Введіть назву продукту - ')
+    # category = input('Введіть назву категорії - ')
+    # amount = input('Введіть кількість - ')
+    # description = input('Введіть опис - ')
+    # price = input('Введіть ціну товара - ')
+    # store = input('Введіть магазин купівлі - ')
+    # csv_columns1 = ['Продукт', 'Категорія', 'Кількість', 'Опис', 'Ціна', 'Магазин']
+    #
+    # dict_data = [
+    #     {'Продукт': product, 'Категорія': category, 'Кількість': amount,
+    #         'Опис': description, 'Ціна': price, 'Магазин': store}
+    # ]
+    #
+    # csv_file = "manager.csv"
+    #
+    # try:
+    #     with open(csv_file, 'w') as csvfile:
+    #         writer = csv.DictWriter(csvfile, delimiter=';', fieldnames=csv_columns1)
+    #         writer.writeheader()
+    #         for data in dict_data:
+    #             writer.writerow(data)
+    #
+    #     with open(csv_file, "r", newline="") as file:
+    #         reader = csv.reader(file)
+    #         for row in reader:
+    #             print(row[0], '-----', row[1], '-----', row[2], '-----', row[3], '-----', row[4], '-----', row[5])
+    # except IOError:
+    #     print("I/O error")
+    # if loginw != write[0]:
+    #     print('Невірний логін')
+    # if passwordw != write[1]:
+    #     print('Невірний пароль')
+    #
+    # else:
+    #     print('Перевірте ввод символів!')
 except Exception as e:
     print(e)
