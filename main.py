0d9e58dec26f57fc0af841b9977e9e39b0ddadd3
peerpressurre <gsophiay@gmail.com>
2022-12-10 12:45:38 +0200

    git c

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import csv
+
 try:
     begin = input('Ви маєте акаунт?(y/n)')
     if begin == 'n':
@@ -46,11 +47,13 @@ try:
 
             with open(csv_file, "r", newline="") as file:
                 reader = csv.reader(file)
+                # проблема з індексами
                 for row in reader:
-                    print(row[0], '-----', row[1], '-----', row[2], '-----', row[3], '-----', row[4], '-----', row[5])
+                    # print(row[0], '-----', row[1], '-----', row[2], '-----', row[3], '-----', row[4], '-----', row[5])
+                    print(row[0], '-----', row[1])
+                    # print(row)
         except IOError:
             print("I/O error")
-
 
     if begin == 'y':
         print('Вхід в акаунт')
@@ -60,12 +63,6 @@ try:
             passwordw = input('Пароль - ')
             datafile = loginw + '.csv'
             print(datafile)
-            print([0])
-            print(datafile[1])
-            print(datafile[4])
-            print(datafile[5])
-
-
 
             if loginw == datafile[0]:
                 if passwordw == datafile[1]:
@@ -74,52 +71,24 @@ try:
                     csv_file = loginw + '_data.csv'
                     with open(csv_file, "r", newline="") as file:
                         reader = csv.reader(file)
+                        # проблема з індексами
                         for row in reader:
                             print(row[0], '-----', row[1], '-----', row[2], '-----', row[3], '-----', row[4], '-----',
                                   row[5])
                 else:
                     print('Невірний пароль. Спробуйте знову')
-                    counter += 1
+
             else:
                 print('Невірний логін. Спробуйте знову')
-                counter += 1
+            # тут каунтер не працює правильно,не знаю куди його
+            counter += 1
         print('Максимальна кількість спроб вичерпана. Спробуйте пізніше.')
         breakpoint()
 
-    product = input('Введіть назву продукту - ')
-    category = input('Введіть назву категорії - ')
-    amount = input('Введіть кількість - ')
-    description = input('Введіть опис - ')
-    price = input('Введіть ціну товара - ')
-    store = input('Введіть магазин купівлі - ')
-    csv_columns1 = ['Продукт', 'Категорія', 'Кількість', 'Опис', 'Ціна', 'Магазин']
-
-    dict_data = [
-        {'Продукт': product, 'Категорія': category, 'Кількість': amount,
-            'Опис': description, 'Ціна': price, 'Магазин': store}
-    ]
-
-    csv_file = "manager.csv"
-
-    try:
-        with open(csv_file, 'w') as csvfile:
-            writer = csv.DictWriter(csvfile, delimiter=';', fieldnames=csv_columns1)
-            writer.writeheader()
-            for data in dict_data:
-                writer.writerow(data)
-
-        with open(csv_file, "r", newline="") as file:
-            reader = csv.reader(file)
-            for row in reader:
-                print(row[0], '-----', row[1], '-----', row[2], '-----', row[3], '-----', row[4], '-----', row[5])
-    except IOError:
+        except IOError:
         print("I/O error")
-    if loginw != write[0]:
-        print('Невірний логін')
-    if passwordw != write[1]:
-        print('Невірний пароль')
 
-    else:
-        print('Перевірте ввод символів!')
+else:
+print('Перевірте ввод символів!')
 except Exception as e:
-    print(e)
+print(e)
