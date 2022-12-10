14b17e7e75fbffbd750bf110a1805959d35368e8
peerpressurre <gsophiay@gmail.com>
2022-12-10 22:07:04 +0200

    git cd

--- main.py
+++ main.py
@@ -45,13 +45,20 @@ try:
                 for data in dict_data:
                     writer.writerow(data)
 
+            read = []
             with open(csv_file, "r", newline="") as file:
                 reader = csv.reader(file)
                 # проблема з індексами
                 for row in reader:
-                    # print(row[0], '-----', row[1], '-----', row[2], '-----', row[3], '-----', row[4], '-----', row[5])
-                    print(row[0], '-----', row[1])
-                    # print(row)
+                    for index in range(0, len(row)):
+                        read.append(row[index])
+
+            for i in read:
+                i.split(';')
+                print(i)
+
+
+
         except IOError:
             print("I/O error")
 
@@ -68,24 +75,77 @@ try:
                 if passwordw == datafile[1]:
                     print('Вхід успішний!')
 
-                    csv_file = loginw + '_data.csv'
-                    with open(csv_file, "r", newline="") as file:
-                        reader = csv.reader(file)
-                        # проблема з індексами
-                        for row in reader:
-                            print(row[0], '-----', row[1], '-----', row[2], '-----', row[3], '-----', row[4], '-----',
-                                  row[5])
+                    print('Оберіть дію:\nПрочитати дані - r\nДодати інформацію - a\nПереписати файл - w')
+                    action = input('->')
+
+                    if action == 'r':
+                        csv_file = loginw + '_data.csv'
+                        with open(csv_file, "r", newline="") as file:
+                            reader = csv.reader(file)
+                            # проблема з індексами
+                            for row in reader:
+                                for index in range(0, len(row)):
+                                    read.append(row[index])
+
+                        for i in read:
+                            i.split(';')
+                            print(i)
+
+                    if action == 'a':
+                        product = input('Введіть назву продукту - ')
+                        category = input('Введіть назву категорії - ')
+                        amount = input('Введіть кількість - ')
+                        description = input('Введіть опис - ')
+                        price = input('Введіть ціну товара - ')
+                        store = input('Введіть магазин купівлі - ')
+                        csv_columns1 = ['Продукт', 'Категорія', 'Кількість', 'Опис', 'Ціна', 'Магазин']
+
+                        dict_data = [
+                            {'Продукт': product, 'Категорія': category, 'Кількість': amount,
+                             'Опис': description, 'Ціна': price, 'Магазин': store}
+                        ]
+                        try:
+
+                            with open(datafile, 'a') as csvfile:
+                                writer = csv.DictWriter(csvfile, delimiter=';', fieldnames=csv_columns)
+                                writer.writeheader()
+                                for data in signup:
+                                    writer.writerow(data)
+                        except IOError:
+                            print('I/P error')
+
+                    if action == 'w':
+
+                        product = input('Введіть назву продукту - ')
+                        category = input('Введіть назву категорії - ')
+                        amount = input('Введіть кількість - ')
+                        description = input('Введіть опис - ')
+                        price = input('Введіть ціну товара - ')
+                        store = input('Введіть магазин купівлі - ')
+                        csv_columns1 = ['Продукт', 'Категорія', 'Кількість', 'Опис', 'Ціна', 'Магазин']
+
+                        dict_data = [
+                            {'Продукт': product, 'Категорія': category, 'Кількість': amount,
+                             'Опис': description, 'Ціна': price, 'Магазин': store}
+                        ]
+
+                        with open(csv_file, 'w') as csvfile:
+                            writer = csv.DictWriter(csvfile, delimiter=';', fieldnames=csv_columns1)
+                            writer.writeheader()
+                            for data in dict_data:
+                                writer.writerow(data)
+
                 else:
+                    counter += 1
                     print('Невірний пароль. Спробуйте знову')
 
             else:
+                counter += 1
                 print('Невірний логін. Спробуйте знову')
-            # тут каунтер не працює правильно,не знаю куди його
-            counter += 1
+
         print('Максимальна кількість спроб вичерпана. Спробуйте пізніше.')
         breakpoint()
 
-    else:
-       print('Перевірте ввод символів!')
 except Exception as ex:
-   print(ex)
+    print(ex)
+
